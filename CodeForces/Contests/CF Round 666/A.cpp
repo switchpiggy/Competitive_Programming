bdd647fece7670dd1dcee422351bccf68951fa89
switchpiggy <switchpiggy@users.noreply.github.com>
2020-08-30 12:01:02 -0700

    cf round 666

--- CodeForces/Contests/CF Round 666/A.cpp
+++ CodeForces/Contests/CF Round 666/A.cpp
@@ -2,7 +2,32 @@
 using namespace std;
 
 typedef long long int ll;
+ll t, n;
+map<char, ll> occ;
 
 int main() {
+    cin >> t;
+    while(t--) {
+        cin >> n;
+        occ.clear();
 
+        string temp;
+        for(ll i = 0; i < n; ++i) {
+            cin >> temp;
+            for(ll j = 0; j < temp.length(); ++j) occ[temp[j]]++;
+        }
+
+        bool ok = 1;
+        for(auto i = occ.begin(); i != occ.end(); ++i) {
+            if(i->second%n != 0) {
+                ok = 0;
+                break;
+            }
+        }
+
+        if(ok) cout <<"YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+    return 0;
 }
--- CodeForces/Contests/CF Round 666/B.cpp
+++ CodeForces/Contests/CF Round 666/B.cpp
@@ -2,7 +2,35 @@
 using namespace std;
 
 typedef long long int ll;
+ll n, a[100007];
+
+#define INF 9223372036854775807;
 
 int main() {
+    cin >> n;
 
+    ll ans = INF
+
+    for(ll i = 0; i < n; ++i) cin >> a[i];
+    sort(a, a + n);
+
+    ll limit = pow(1e18, 1.0/n);
+
+    ll res = 0;
+    for(ll c = 1; c <= limit; ++c) {
+        res++;
+        ll cnt = 0, cur = 1;
+        for(ll i = 0; i < n; ++i) {
+            cnt += abs(a[i] - cur);
+            cur *= c;
+        }
+
+        //cout << c << endl;
+
+        ans = min(cnt, ans);
+    }
+
+    cout << ans << endl;
+
+    return 0;
 }
