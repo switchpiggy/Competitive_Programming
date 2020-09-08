fdf945ffc3ac73ced8ebea18b300dd26c01e0e7e
switchpiggy <switchpiggy@users.noreply.github.com>
2020-09-07 17:09:35 -0700

    cp grind

--- Codechef/Codechef September Long Challenge/C.cpp
+++ Codechef/Codechef September Long Challenge/C.cpp
@@ -2,25 +2,27 @@
 using namespace std;
 
 typedef long long int ll;
-ll t, n, adj[6][6], ve[6];
+ll t, n;
+long double adj[6][6], ve[6];
 
 ll djikstra(ll x) {
-    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+    priority_queue<pair<long double, ll>, vector<pair<ll, ll>>, greater<pair<long double, ll>>> pq;
     pq.push(make_pair(0, x));
 
-    ll dist[6];
+    long double dist[6];
     for(ll i = 0; i < 6; ++i) dist[i] = INT_MAX;
 
     //cout << dist[0] << endl;
 
     dist[x] = 0;
     while(!pq.empty()) {
-        pair<ll, ll>t = pq.top();
+        pair<long double, ll>t = pq.top();
         pq.pop();
+        //cout << t.second << endl;
 
         if(t.first > dist[t.second]) continue;
         for(ll i = 1; i <= n; ++i) {
-            if(adj[t.second][i] != -1 && (adj[t.second][i] >= dist[t.second] || dist[t.second] == INT_MAX)) {
+            if(adj[t.second][i] != -1.0 && (adj[t.second][i] >= dist[t.second] || dist[t.second] == INT_MAX)) {
                 if(adj[t.second][i] < dist[i]) {
                     dist[i] = adj[t.second][i];
                     pq.push(make_pair(dist[i], i));
@@ -43,21 +45,18 @@ int main() {
 
     cin >> t;
     while(t--) {
-        memset(adj, -1, sizeof(adj[0][0]) * 6 * 6);
+        memset(adj, -1.0, sizeof(adj[0][0]) * 6 * 6);
+
         cin >> n;
         for(ll i = 1; i <= n; ++i) {
             cin >> ve[i];
         }
 
-        for(ll i = 1; i <= 50; ++i) {
-            for(ll j = 1; j <= n; ++j) {
-                for(ll k = j + 1; k <= n; ++k) {
-                    if(j + ve[j] * i == k + ve[k] * i) {
-                        if(adj[j][k] == -1) {
-                            adj[j][k] = adj[k][j] = i;
-                        }
-                    }
-                }
+        for(ll i = 1; i <= n; ++i) {
+            for(ll j = i + 1; j <= n; ++j) {
+                if(ve[j] >= ve[i]) continue;
+                long double k = (j - i)/(ve[i] - ve[j]);
+                adj[i][j] = adj[j][i] = k;
             }
         }
 
@@ -67,9 +66,10 @@ int main() {
         }
         cout << endl;*/
 
-        ll maxn = 1, minn = INT_MAX;
+        ll maxn = 0, minn = LONG_LONG_MAX;
         for(ll i = 1; i <= n; ++i) {
             ll cur = djikstra(i);
+            //cout << endl;
             maxn = max(cur, maxn);
             minn = min(cur, minn);
         }
