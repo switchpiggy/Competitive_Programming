2bb73b9c41ac99eb0d661055167076524543c8db
switchpiggy <switchpiggy@users.noreply.github.com>
2020-10-20 10:15:07 -0700

    cp grind

--- CodeForces/Contests/CF 677Div 3/F.cpp
+++ CodeForces/Contests/CF 677Div 3/F.cpp
@@ -24,11 +24,18 @@ int main() {
             mod[i][j] = -100000000;
         }
     }
+    mod[0][0] = 0;
     for (ll i = 0; i < n; i ++) {
         for (ll j = 0; j < m; j ++) {
+            mod[i][ar[i][j]%k] = max(mod[i][ar[i][j]%k], ar[i][j]);
             for (ll l = j + 1; l < m; l ++) {
                 mod[i][(ar[i][j] + ar[i][l])%k] = max(mod[i][(ar[i][j] + ar[i][l])%k], (ar[i][j] + ar[i][l]));
             }
+        }
+    }
+    for (int i = 1; i < n; i ++) {
+        for (int j = 0; j < k; j ++) {
+            mod[i][j] = max(mod[i][j],mod[i-1][j]);
         }
     }
     ll dp[n][k];
@@ -45,16 +52,15 @@ int main() {
             for (ll l = 0; l < k; l ++) {
                 if (dp[i-1][j] != -100000000 && mod[i][l] != -100000000)
                 dp[i][(j+l)%k] = max(dp[i][(j+l)%k], dp[i-1][j] + mod[i][l]);
+                
             }
         }
     }
-    cout << dp[n-1][0] << endl;
+    cout << max((ll)0, dp[n-1][0]) << endl;
     // for (ll i = 0; i < n; i ++) {
     //     for (ll j = 0; j < k; j ++) {
     //         cout << dp[i][j] << " ";
     //     }
     //     cout << endl;
     // }
-
-    return 0;
 }
