0ca80754c23f2f4473f41abbb7c18e2be9535545
switchpiggy <switchpiggy@users.noreply.github.com>
2020-12-06 13:20:05 -0800

    cp grind

--- CodeForces/Contests/CF Global Round 12/D.cpp
+++ CodeForces/Contests/CF Global Round 12/D.cpp
@@ -2,41 +2,61 @@
 using namespace std;
 
 typedef long long int ll;
-ll t, n, a[300007], cnt[300007], l[300007], r[300007];
-bool ok[300007];
-map<pair<ll, ll>, bool> found;
+ll t, n, a[300007];
+multiset<ll> s;
+
+bool check(ll x) {
+    s.clear();
+    vector<ll> v;
+    //ll mins = LONG_LONG_MAX;
+    for(ll i = 1; i <= x; ++i) {
+        s.insert(a[i]);
+    }
+    v.push_back(*s.begin());
+
+    for(ll i = x + 1; i <= n; ++i) {
+        s.erase(s.find(a[i - x]));
+        s.insert(a[i]);
+        v.push_back(*s.begin());
+    }
+
+    sort(v.begin(), v.end());
+    for(ll i = 0; i < (ll)v.size(); ++i) {
+        if(v[i] != i + 1) return 0;
+    }
+
+    return 1;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cin >> t;
     while(t--) {
-        found.clear();
         cin >> n;
         for(ll i = 1; i <= n; ++i) cin >> a[i];
-        memset(cnt, 0, sizeof(cnt));
-        memset(ok, 1, sizeof(ok));
+        ll l = 1, r = n + 1, ans = INT_MAX;
 
-        for(ll i = 0; i <= n; ++i) {
-            l[i] = 0, r[i] = n + 1;
+        while(l < r) {
+            ll m = (l + r)/2;
+            if(check(m)) {
+                ans = m;
+                r = m;
+            } else l = m + 1;
         }
 
-        stack<ll> s;
-        for(ll i = 1; i <= n; ++i) {
-            while(!s.empty() && a[s.top()] >= a[i]) s.pop();
-            if(!s.empty()) l[i] = s.top();
-            s.push(i);
+        sort(a + 1, a + n + 1);
+        bool ok = 1;
+        for(ll i = 1; i < n; ++i) {
+            if(a[i] == a[i + 1]) {
+                ok = 0;
+                break;
+            }
         }
 
-        while(!s.empty()) s.pop();
-
-        for(ll i = n; i >= 1; --i) {
-            while(!s.empty() && a[s.top()] >= a[i]) s.pop();
-            if(!s.empty()) r[i] = s.top();
-            s.push(i);
-        }
-
-        
+        cout << ok;
+        for(ll i = 2; i <= n; ++i) if(i >= ans) cout << 1;
+        else cout << 0;
 
         cout << '\n';
     }
