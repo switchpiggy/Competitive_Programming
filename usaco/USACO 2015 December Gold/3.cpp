cda98dc8fc0f8442100d38f2a5552ccd2d2d3fc9
switchpiggy <switchpiggy@users.noreply.github.com>
2020-12-07 14:49:14 -0800

    cp grind

--- usaco/USACO 2015 December Gold/3.cpp
+++ usaco/USACO 2015 December Gold/3.cpp
@@ -2,37 +2,86 @@
 using namespace std;
 
 typedef long long int ll;
-ll n, m, a[1007][1007], dist[1007][1007], dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
-priority_queue<pair<ll, pair<pair<ll, ll>, pair<ll, ll>>>, vector<pair<ll, pair<pair<ll, ll>, pair<ll, ll>>>>, greater<pair<ll, pair<pair<ll, ll>, pair<ll, ll>>>>> pq;
+ll dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1}, n, m, dist[10000001];
 
-void dijkstra() {
-    pq.push({0, {{0, 0}, {0, 0}}});
-    dist[0][0] = 0;
-    while(!pq.empty()) {
-        pair<ll, pair<pair<ll, ll>, pair<ll, ll>>> p = pq.top();
-        pq.pop();
+struct state {
+    ll x, y, dir;
+    bool smell;
 
-        if(p.first > dist[p.second.first.first][p.second.first.second]) continue;
-        ll x = p.second.first.first, y = p.second.first.second, slide = p.second.second.second, smell = p.second.second.first;
-        for(ll i = 0; i < 4; ++i) {
-            if(dx[i] + x >= n || dx[i] + x < 0 || dy[i] + y >= m || dy[i] + m < 0) continue;
-            if(smell && a[dx[i] + x][dy[i] + y]) 
-        }
+    ll comp() {
+        return smell + 2 * dir + 10 * y + 10000 * x;
     }
-}
+
+    state(ll g, ll b, ll c, bool d) {
+        x = g;
+        y = b;
+        dir = c;
+        smell = d;
+    }
+
+    static state decomp(ll x) {
+        return state(x/10000, (x/10)%1000, (x/2)%5, x%2);
+    }
+
+    ll cell(const vector<vector<ll>>& a, ll c, ll b) {
+        if(c < 0 || c >= n || b < 0 || b >= m) return -1;
+        return a[c][b];
+    }
+};
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    freopen("dream.in", "r", stdin);
+    freopen("dream.out", "w", stdout);
+
     cin >> n >> m;
+    vector<vector<ll>> a(n, vector<ll>(m));
     for(ll i = 0; i < n; ++i) {
         for(ll j = 0; j < m; ++j) cin >> a[i][j];
     }
 
-    dijkstra();
+    for(ll i = 0; i <= 10000000; ++i) dist[i] = -1;
+    queue<ll> q;
 
-    if(!dist[n - 1][m - 1]) cout << "-1\n";
-    else cout << dist[n - 1][m - 1] << '\n';
+    ll init = state(0, 0, 0, 0).comp();
+    q.push(init);
+    dist[init] = 0;
 
+    while(!q.empty()) {
+        ll f = q.front();
+        state st = state::decomp(f);
+        q.pop();
+
+        if(st.x == n - 1 && st.y == m - 1) {
+            cout << dist[st.comp()] << '\n';
+            return 0;
+        }
+
+        if(a[st.x][st.y] == 4 && st.dir) {
+            ll cur = st.cell(a, st.x + dx[st.dir - 1], st.y + dy[st.dir - 1]);
+            if(cur != -1 && cur != 3 && cur != 0) {
+                state nxt = state(st.x + dx[st.dir - 1], st.y + dy[st.dir - 1], st.dir, cur == 2);
+                if(dist[nxt.comp()] != -1) continue;
+                dist[nxt.comp()] = dist[st.comp()] + 1;
+                q.push(nxt.comp());
+                continue;
+            }
+        }
+
+        for(ll i = 0; i < 4; ++i) {
+            ll cur = st.cell(a, st.x + dx[i], st.y + dy[i]);
+            if(cur == 3 && !st.smell) continue;
+            if(cur == -1 || cur == 0) continue;
+
+            state nxt = state(st.x + dx[i], st.y + dy[i], i + 1, (st.smell || cur == 2) && cur != 4);
+            if(dist[nxt.comp()] != -1) continue;
+            dist[nxt.comp()] = dist[st.comp()] + 1;
+            q.push(nxt.comp()); 
+        }
+
+    }
+
+    cout << "-1\n";
     return 0;
 }
